19f0a695258ce3c35483e794cdff51dccc5a91e5
r4coder <ritheeshk2003@gmail.com>
2026-09-21 17:42:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+const form=document.getElementbyId("f");
+const name=document.getElementbyId("name");
+const age=document.getElementbyId("age");
+
+form.addEventListener("submit",()=>{
+
+	if(name.value===""||age.value==="")
+		{
+			alert("Please enter valid details");
+        }
+	
+});
+
+promise1=new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+		  resolve("Welcome,You can vote."); 
+	  },40000)
+		  
+	});
+
+promise2=new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+		  resolve("Oh sorry.You aren't old enough"); 
+	  },40000)
+		  
+	});
+
+if(age>18)
+{
+	promise1.then((data1)=>{
+	console.log(data1)
+});
+	
+}
+
+else 
+{
+  promise2.then((data2)=>{
+	console.log(data2)
+});
+	
+}
+
+
+
+
